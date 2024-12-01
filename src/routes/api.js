09bb290180a2c6d5814c5f23718d8990f51e82e3
physicalculture <physicalculture07@gmail.com>
2024-12-01 13:54:50 +0530

    route version 2 update

--- src/routes/api.js
+++ src/routes/api.js
@@ -9,6 +9,13 @@ router.post("/login", apiController.login);
 router.post("/verifyotp", apiController.verifyOtp);
 router.post("/forgotpassword", apiController.forgotPassword);
 router.post("/resetpassword", apiController.resetPassword);
+
+router.post("/v2/signup", apiController.signUp);
+router.post("/v2/login", apiController.login);
+router.post("/v2/verifyotp", apiController.verifyOtp);
+router.post("/v2/forgotpassword", apiController.forgotPassword);
+router.post("/v2/resetpassword", apiController.resetPassword);
+
 // router.get("/user_profile",isAuth, apiController.userProfile);
 // router.post("/update_user_profile",isAuth, apiController.updateUserProfile);
 // router.get("/all_courses",isAuth, apiController.getAllCourses)
@@ -33,6 +40,19 @@ router.get("/all_banner", apiController.getAllBanners)
 router.post("/buy_course", apiController.buyCourse)
 router.post("/download_class_video", apiController.downloadClassVideo);
 router.get("/contactus", apiController.contactUs);
+
+router.get("/v2/user_profile", isAuth, apiController.userProfile);
+router.post("/v2/update_user_profile", isAuth, apiController.updateUserProfile);
+router.get("/v2/all_courses", isAuth, apiController.getAllCourses)
+router.get("/v2/get_classesbycourse/:id",isAuth,  apiController.getClassByCourseId)
+router.get("/v2/all_notes",isAuth,  apiController.getAllPdfNotes)
+router.get("/v2/all_previous_papers",isAuth,  apiController.getAllPreviousPapers)
+router.get("/v2/all_syllabus",isAuth, apiController.getAllSyllabus)
+router.get("/v2/all_testseries", isAuth,apiController.getAllTestSeries)
+router.get("/v2/all_banner", apiController.getAllBanners)
+router.post("/v2/buy_course",isAuth, apiController.buyCourse)
+router.post("/v2/download_class_video", apiController.downloadClassVideo);
+router.get("/v2/contactus", apiController.contactUs);
 
 
 
